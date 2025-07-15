4f9bee73c6227ea87f3d3429ae9ca7124ace8915
Dominik Hendrix <dominik.hendrix@clickfood.de>
2025-07-15 07:41:40 +0200

    set cookies not httponly

--- src/Http/Controllers/CookieConsentController.php
+++ src/Http/Controllers/CookieConsentController.php
@@ -33,7 +33,7 @@ class CookieConsentController extends Controller
                 $blockIds[] = $cookieBlock->id;
             }
 
-            $cookie = Cookie::make(config('twill-cookie-consent.cookie_name'), json_encode($blockIds), 60 * 24 * 365);
+            $cookie = Cookie::make(config('twill-cookie-consent.cookie_name'), json_encode($blockIds), 60 * 24 * 365, null, null, null, false);
         }
 
         return redirect()->back()->withCookie($cookie);
